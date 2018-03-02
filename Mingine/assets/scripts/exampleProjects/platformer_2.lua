8e6793ac2d313d5a3eff9613df5bf4aec04fed2e
James <jmarshallstewart@gmail.com>
2018-03-01 21:06:44 -0600

    walkability grid now allocated only if tmx contains an object layer named NoWalk (and can now support multiple NoWalk layers). WIP Platformer_2 project.

---
+++ Mingine/assets/scripts/exampleProjects/platformer_2.lua
@@ -0,0 +1,217 @@
+-- This projects ports the excellent tutorial found here to mingine:
+--
+-- https://codeincomplete.com/posts/tiny-platformer/
+--
+-- This tutorial is well worth reading. Though it is written in ActionScript,
+-- the concepts are portable to any development environment used for making games.
+--
+-- This project loads a tmx file, but only uses the first layer.
+--
+-- This project does not rely on the mingine entity system, and instead it
+-- implements a separate physics sim more amenable to platforming gameplay.
+--
+-- The player keeps a redundant copy of tuning constants so that UpdateActor()
+-- can be written in anticipation of adding different types of actors later that
+-- may have different tuning values so that they behave differently from the player.
+
+
+--tuning parameters for player input
+
+GRAVITY = 1.2
+MAX_SPEED = { x = 3.4375, y = 6.3 }
+LINEAR_ACCELERATION = 0.625 -- how fast the player accelerates when walking
+FRICTION = 2 
+JUMP_IMPULSE = 25
+FALLING_FRICTION_SCALE = 0.5
+FALLING_ACCELERATION_SCALE = 0.5
+SETTLE_TOLERANCE = 0.11 -- higher number indicates max penetration for player to snap to grid when coming to a stop.
+
+
+-- helper functions
+
+function getCell(x, y)
+    return map.tiles[1][(x + (y * map.width)) + 1] -- +1 because of lua array indexing
+end
+
+function SetActor(actor, x, y)
+    actor.x = x
+    actor.y = y
+    actor.left = false
+    actor.right = false
+    actor.jump = false
+    actor.jumping = false
+    actor.falling = false
+    actor.velocity = { x = 0, y = 0 }
+    actor.acceleration = { x = 0, y = 0 }
+    actor.friction = FRICTION
+    actor.linearAcceleration = LINEAR_ACCELERATION
+    actor.maxVelocityX = MAX_SPEED.x
+    actor.maxVelocityY = MAX_SPEED.y
+    actor.jumpImpulse =  JUMP_IMPULSE
+    actor.monster = false
+end
+
+function UpdatePlayerInput()
+    player.left = false
+    player.right = false
+    player.jump = false
+
+    if IsKeyDown(SDL_SCANCODE_LEFT) then player.left = true end
+    if IsKeyDown(SDL_SCANCODE_RIGHT) then player.right = true end
+    if IsKeyDown(SDL_SCANCODE_SPACE) then player.jump = true end
+end
+
+function UpdateActor(actor)
+    local wasLeft = actor.velocity.x < 0
+    local wasRight = actor.velocity.x > 0
+    local falling = actor.falling
+              
+    local friction = actor.friction * Pick(falling, FALLING_FRICTION_SCALE, 1.0)
+    local acceleration = actor.linearAcceleration * Pick(falling, FALLING_ACCELERATION_SCALE, 1.0)
+    
+    actor.acceleration.x = 0
+    actor.acceleration.y = GRAVITY
+    
+    if actor.left then actor.acceleration.x = actor.acceleration.x - acceleration
+    elseif wasLeft then actor.acceleration.x = actor.acceleration.x + friction end
+    
+    if actor.right then actor.acceleration.x = actor.acceleration.x + acceleration
+    elseif wasRight then actor.acceleration.x = actor.acceleration.x - friction end
+    
+    --start jumping?
+    if actor.jump and not actor.jumping and not falling then
+        actor.acceleration.y = actor.acceleration.y - actor.jumpImpulse
+        actor.jumping = true
+    end
+    
+    local dt = GetFrameTime() / 100.0
+    
+    actor.x = actor.x + actor.velocity.x * dt
+    actor.y = actor.y + actor.velocity.y * dt
+    actor.velocity.x = Clamp(actor.velocity.x + actor.acceleration.x * dt, -actor.maxVelocityX, actor.maxVelocityX)
+    actor.velocity.y = Clamp(actor.velocity.y + actor.acceleration.y * dt, -actor.maxVelocityY, actor.maxVelocityY)
+    
+    --clamp x velocity to prevent jiggle when changing directions
+    if (wasLeft and actor.velocity.x > 0) or (wasRight and actor.velocity.x < 0) then 
+        actor.velocity.x = 0
+    end
+    
+    local tileX = math.floor(actor.x)
+    local tileY = math.floor(actor.y) 
+    
+    -- how deeply are we penetrating other tiles?
+    nx = math.fmod(actor.x, 1)
+    ny = math.fmod(actor.y, 1)
+    
+    local cell = getCell(tileX, tileY)
+    local cellRight = getCell(tileX + 1, tileY)
+    local cellDown = getCell(tileX, tileY + 1)
+    local cellDiag = getCell(tileX + 1, tileY + 1)
+    
+    if Contains(map.walkable, cell) then cell = nil end
+    if Contains(map.walkable, cellRight) then cellRight = nil end
+    if Contains(map.walkable, cellDown) then cellDown = nil end
+    if Contains(map.walkable, cellDiag) then cellDiag = nil end
+            
+    if actor.velocity.y > 0 then
+        if (cellDown and not cell) or (cellDiag and not cellRight and nx > 0) then
+            actor.y = tileY
+            actor.velocity.y = 0
+            actor.falling = false
+            actor.jumping = false
+            ny = 0
+        end
+    elseif actor.velocity.y < 0 then
+        if (cell and not cellDown) or (cellRight and not cellDiag and nx > 0) then
+            actor.y = tileY + 1
+            actor.velocity.y = 0
+            cell = cellDown
+            cellRight = cellDiag
+            ny = 0
+        end
+    end
+    
+    if actor.velocity.x > 0 then
+        if (cellRight and not cell) or (cellDiag and not cellDown and ny > 0) then
+            actor.x = tileX
+            actor.velocity.x = 0
+        end
+    elseif actor.velocity.x < 0 then
+        if (cell and not cellRight) or (cellDown and not cellDiag and ny > 0) then
+            actor.x = tileX + 1
+            actor.velocity.x = 0
+        end
+    end
+       
+    actor.falling = not (cellDown or (nx > 0 and cellDiag))
+    
+    -- let the actor tend to settle aligned to the grid so that it can fit through one-tile holes.
+    if actor.velocity.x == 0 and actor.velocity.y == 0 and math.fmod(actor.x, 1) < SETTLE_TOLERANCE then
+        actor.x = math.floor(actor.x)
+    end
+    
+    -- bounce monster if it as a platform edge.
+    if actor.monster then
+        if actor.left and (cell or not cellDown) then
+            actor.left = false
+            actor.right = true
+        elseif actor.right and (cellRight or not cellDiag) then
+            actor.left = true
+            actor.right = false
+        end
+    end
+end
+
+function UpdateMonsters()
+    for i = 1, #monsters do
+        UpdateActor(monsters[i])
+    end
+end
+
+function DrawWorld()
+    for y = 0, map.height - 1 do
+        for x = 0, map.width - 1 do
+            local tileImageIndex = map.tiles[1][(x + y * map.width) + 1]
+            DrawImageFrame(tileImage, x * map.tileSize, y * map.tileSize, map.tileSize, map.tileSize, tileImageIndex - 1, 0, 1)
+        end
+    end
+end
+
+function DrawPlayer()
+    SetDrawColor(255, 255, 255, 255)
+    FillRect(player.x * map.tileSize, player.y * map.tileSize, map.tileSize, map.tileSize)
+end
+
+-- core functions
+
+function Start()
+    LoadTmxFile(assetDirectory .. "maps/platformer.tmx")
+    
+    -- This example can handle maps with different tile sizes. For example, comment out the previous LoadTmxFile() call and uncomment this:
+    -- LoadTmxFile(assetDirectory .. "maps/platformer32.tmx")
+    
+    map.walkable = {1, 2} --indices of tiles that the player and other actors can walk through.
+
+    map.tileSize = map.tileSize
+    CreateWindow(map.width * map.tileSize, map.height * map.tileSize)
+    SetWindowTitle("Platformer")
+    
+    tileImage = LoadImage(map.tileAtlas)
+    player = {}
+    SetActor(player, 10, 22)
+    
+    monsters = {}
+    treasures = {}
+end
+
+function Update()
+    UpdatePlayerInput()
+    UpdateActor(player)
+    
+    UpdateMonsters()
+end
+
+function Draw()
+    DrawWorld()
+    DrawPlayer()
+end
--- Mingine/src/tmxToLua.cpp
+++ Mingine/src/tmxToLua.cpp
@@ -12,186 +12,182 @@ using namespace tinyxml2;
 
 namespace mingine {
 
-extern char stringBuilderBuffer[MAX_STRING];
+	extern char stringBuilderBuffer[MAX_STRING];
 
-class MapData
-{
-public:
-	~MapData() { delete[] walkabilityGrid; }
-	int tileSize{};
-	int width{};
-	int height{};
-	const char* tileSetPath{ nullptr };
-	const char* outTableName{ nullptr };
-	vector<vector<int>> tiles{};
-	bool* walkabilityGrid{ nullptr };
-
-	int mapLength() const { return width * height; }
-};
-
-void XMLCheckResult(int result)
-{
-	if (result != XML_SUCCESS)
+	class MapData
 	{
-		snprintf(stringBuilderBuffer, sizeof(stringBuilderBuffer), "XML Error: %i", result);
-		log(&stringBuilderBuffer[0]);
-		showErrorBox(&stringBuilderBuffer[0]);
-	}
-}
+	public:
+		~MapData() { delete[] walkabilityGrid; }
+		int tileSize{};
+		int width{};
+		int height{};
+		const char* tileSetPath{ nullptr };
+		const char* outTableName{ nullptr };
+		vector<vector<int>> tiles{};
+		bool* walkabilityGrid{ nullptr };
 
-int readIntAttribute(XMLElement* element, const char* intName)
-{
-	const char * szAttributeText = element->Attribute(intName);
-	stringstream strValue;
-	strValue << szAttributeText;
+		int mapLength() const { return width * height; }
+	};
 
-	int value;
-	strValue >> value;
-
-	return value;
-}
-
-void writeStringField(const char* outTableName, const char* valueName, const char* value, string& outString)
-{
-	outString += string(outTableName) + "." + valueName + " = \"" + value + "\"\n";
-}
-
-void writeIntField(const char* outTableName, const char* valueName, int value, string& outString)
-{
-	outString += string(outTableName) + "." + valueName + " = " + to_string(value) + "\n";
-}
-
-void writeLuaMapScript(const MapData& mapData, string& outScript)
-{
-	outScript = outScript.append(mapData.outTableName).append(" = {}\n");
-	writeStringField(mapData.outTableName, "tileAtlas", mapData.tileSetPath, outScript);
-	writeIntField(mapData.outTableName, "tileSize", mapData.tileSize, outScript);
-	writeIntField(mapData.outTableName, "width", mapData.width, outScript);
-	writeIntField(mapData.outTableName, "height", mapData.height, outScript);
-
-	outScript.append(mapData.outTableName).append(".tiles = {}\n");
-
-	for (size_t layerIndex = 0; layerIndex < mapData.tiles.size(); ++layerIndex)
+	void XMLCheckResult(int result)
 	{
-		outScript += string(mapData.outTableName) + ".tiles[" + to_string(layerIndex + 1) + "] = {";
-
-		for (const int& i : mapData.tiles[layerIndex])
+		if (result != XML_SUCCESS)
 		{
-			outScript += to_string(i) + ",";
+			snprintf(stringBuilderBuffer, sizeof(stringBuilderBuffer), "XML Error: %i", result);
+			log(&stringBuilderBuffer[0]);
+			showErrorBox(&stringBuilderBuffer[0]);
 		}
-
-		// erase last comma
-		outScript.resize(outScript.size() - 1);
-		outScript += "}\n"; // end of tiles
 	}
 
-	if (mapData.walkabilityGrid != nullptr)
+	int readIntAttribute(XMLElement* element, const char* intName)
 	{
-		outScript += string(mapData.outTableName) + ".walkabilityGrid = {";
+		const char * szAttributeText = element->Attribute(intName);
+		stringstream strValue;
+		strValue << szAttributeText;
 
-		for (int i = 0; i < mapData.mapLength(); ++i)
+		int value;
+		strValue >> value;
+
+		return value;
+	}
+
+	void writeStringField(const char* outTableName, const char* valueName, const char* value, string& outString)
+	{
+		outString += string(outTableName) + "." + valueName + " = \"" + value + "\"\n";
+	}
+
+	void writeIntField(const char* outTableName, const char* valueName, int value, string& outString)
+	{
+		outString += string(outTableName) + "." + valueName + " = " + to_string(value) + "\n";
+	}
+
+	void writeLuaMapScript(const MapData& mapData, string& outScript)
+	{
+		outScript = outScript.append(mapData.outTableName).append(" = {}\n");
+		writeStringField(mapData.outTableName, "tileAtlas", mapData.tileSetPath, outScript);
+		writeIntField(mapData.outTableName, "tileSize", mapData.tileSize, outScript);
+		writeIntField(mapData.outTableName, "width", mapData.width, outScript);
+		writeIntField(mapData.outTableName, "height", mapData.height, outScript);
+
+		outScript.append(mapData.outTableName).append(".tiles = {}\n");
+
+		for (size_t layerIndex = 0; layerIndex < mapData.tiles.size(); ++layerIndex)
 		{
-			outScript += (mapData.walkabilityGrid[i] ? "1," : "0,");
-		}
+			outScript += string(mapData.outTableName) + ".tiles[" + to_string(layerIndex + 1) + "] = {";
 
-		// erase last comma
-		outScript.resize(outScript.size() - 1);
-		outScript += "}\n"; // end of walkability grid
-	}
-}
-
-int parseTmx(const char* tmxFile, const char* topPathToMatch, const char* outTableName, string& outScript)
-{
-	XMLDocument xmlDoc;
-	XMLError result = xmlDoc.LoadFile(tmxFile);
-	XMLCheckResult(result);
-
-	MapData mapData;
-	mapData.outTableName = outTableName;
-
-	XMLElement* pMapElement = xmlDoc.RootElement();
-	if (pMapElement == nullptr) return XML_ERROR_FILE_READ_ERROR;
-
-	// parse tilesets
-	XMLElement* pTilesetElement = pMapElement->FirstChildElement("tileset");
-	if (pTilesetElement == nullptr) return XML_ERROR_PARSING_ELEMENT;
-
-	while (pTilesetElement != nullptr)
-	{
-		const char * name = pTilesetElement->Attribute("name");
-		if (name == nullptr) return XML_ERROR_PARSING_ATTRIBUTE;
-		
-		snprintf(stringBuilderBuffer, sizeof(stringBuilderBuffer), "Parsing tileset %s", name);
-		log(stringBuilderBuffer);
-
-		int tileWidth = readIntAttribute(pTilesetElement, "tilewidth");
-		int tileHeight = readIntAttribute(pTilesetElement, "tileheight");
-
-		assert(tileWidth == tileHeight);
-		mapData.tileSize = tileWidth;
-				
-		snprintf(stringBuilderBuffer, sizeof(stringBuilderBuffer), "Tile Width: %i Tile Height %i", tileWidth, tileHeight);
-		log(stringBuilderBuffer);
-
-		XMLElement* pImageElement = pTilesetElement->FirstChildElement("image");
-		const char* source = pImageElement->Attribute("source");
-		string s = string(source);
-		int index = s.find(topPathToMatch);
-		mapData.tileSetPath = &source[index];
-		
-		snprintf(stringBuilderBuffer, sizeof(stringBuilderBuffer), "Tile atlas source: %s", mapData.tileSetPath);
-		log(stringBuilderBuffer);
-
-		pTilesetElement = pTilesetElement->NextSiblingElement("tileset");
-	}
-
-	// parse layers
-	XMLElement* pLayerElement = pMapElement->FirstChildElement("layer");
-	if (pLayerElement == nullptr) return XML_ERROR_PARSING_ELEMENT;
-
-	while (pLayerElement != nullptr)
-	{
-		const char * name = pLayerElement->Attribute("name");
-		if (name == nullptr) return XML_ERROR_PARSING_ATTRIBUTE;
-		
-		snprintf(stringBuilderBuffer, sizeof(stringBuilderBuffer), "Parsing layer %s", name);
-		log(stringBuilderBuffer);
-
-		mapData.width = readIntAttribute(pLayerElement, "width");
-		mapData.height = readIntAttribute(pLayerElement, "height");
-				
-		snprintf(stringBuilderBuffer, sizeof(stringBuilderBuffer), "Map width: %i Map height: %i", mapData.width, mapData.height);
-		log(stringBuilderBuffer);
-
-		XMLElement* pDataElement = pLayerElement->FirstChildElement("data");
-
-		stringstream ss(pDataElement->GetText());
-
-		int numLayers = mapData.tiles.size();
-		vector<int> v;
-		mapData.tiles.push_back(v);
-
-		int i;
-
-		while (ss >> i)
-		{
-			mapData.tiles[numLayers].push_back(i);
-
-			if (ss.peek() == ',')
+			for (const int& i : mapData.tiles[layerIndex])
 			{
-				ss.ignore();
+				outScript += to_string(i) + ",";
 			}
+
+			// erase last comma
+			outScript.resize(outScript.size() - 1);
+			outScript += "}\n"; // end of tiles
 		}
-				
-		pLayerElement = pLayerElement->NextSiblingElement("layer");
+
+		if (mapData.walkabilityGrid != nullptr)
+		{
+			outScript += string(mapData.outTableName) + ".walkabilityGrid = {";
+
+			for (int i = 0; i < mapData.mapLength(); ++i)
+			{
+				outScript += (mapData.walkabilityGrid[i] ? "1," : "0,");
+			}
+
+			// erase last comma
+			outScript.resize(outScript.size() - 1);
+			outScript += "}\n"; // end of walkability grid
+		}
 	}
 
-	// parse object groups
-	XMLElement* pObjectGroupElement = pMapElement->FirstChildElement("objectgroup");
-	if (pObjectGroupElement != nullptr)
+	int parseTmx(const char* tmxFile, const char* topPathToMatch, const char* outTableName, string& outScript)
 	{
-		mapData.walkabilityGrid = new bool[mapData.mapLength()];
-		memset(mapData.walkabilityGrid, 1, mapData.mapLength());
+		XMLDocument xmlDoc;
+		XMLError result = xmlDoc.LoadFile(tmxFile);
+		XMLCheckResult(result);
+
+		MapData mapData;
+		mapData.outTableName = outTableName;
+
+		XMLElement* pMapElement = xmlDoc.RootElement();
+		if (pMapElement == nullptr) return XML_ERROR_FILE_READ_ERROR;
+
+		// parse tilesets
+		XMLElement* pTilesetElement = pMapElement->FirstChildElement("tileset");
+		if (pTilesetElement == nullptr) return XML_ERROR_PARSING_ELEMENT;
+
+		while (pTilesetElement != nullptr)
+		{
+			const char * name = pTilesetElement->Attribute("name");
+			if (name == nullptr) return XML_ERROR_PARSING_ATTRIBUTE;
+
+			snprintf(stringBuilderBuffer, sizeof(stringBuilderBuffer), "Parsing tileset %s", name);
+			log(stringBuilderBuffer);
+
+			int tileWidth = readIntAttribute(pTilesetElement, "tilewidth");
+			int tileHeight = readIntAttribute(pTilesetElement, "tileheight");
+
+			assert(tileWidth == tileHeight);
+			mapData.tileSize = tileWidth;
+
+			snprintf(stringBuilderBuffer, sizeof(stringBuilderBuffer), "Tile Width: %i Tile Height %i", tileWidth, tileHeight);
+			log(stringBuilderBuffer);
+
+			XMLElement* pImageElement = pTilesetElement->FirstChildElement("image");
+			const char* source = pImageElement->Attribute("source");
+			string s = string(source);
+			int index = s.find(topPathToMatch);
+			mapData.tileSetPath = &source[index];
+
+			snprintf(stringBuilderBuffer, sizeof(stringBuilderBuffer), "Tile atlas source: %s", mapData.tileSetPath);
+			log(stringBuilderBuffer);
+
+			pTilesetElement = pTilesetElement->NextSiblingElement("tileset");
+		}
+
+		// parse layers
+		XMLElement* pLayerElement = pMapElement->FirstChildElement("layer");
+		if (pLayerElement == nullptr) return XML_ERROR_PARSING_ELEMENT;
+
+		while (pLayerElement != nullptr)
+		{
+			const char * name = pLayerElement->Attribute("name");
+			if (name == nullptr) return XML_ERROR_PARSING_ATTRIBUTE;
+
+			snprintf(stringBuilderBuffer, sizeof(stringBuilderBuffer), "Parsing layer %s", name);
+			log(stringBuilderBuffer);
+
+			mapData.width = readIntAttribute(pLayerElement, "width");
+			mapData.height = readIntAttribute(pLayerElement, "height");
+
+			snprintf(stringBuilderBuffer, sizeof(stringBuilderBuffer), "Map width: %i Map height: %i", mapData.width, mapData.height);
+			log(stringBuilderBuffer);
+
+			XMLElement* pDataElement = pLayerElement->FirstChildElement("data");
+
+			stringstream ss(pDataElement->GetText());
+
+			int numLayers = mapData.tiles.size();
+			vector<int> v;
+			mapData.tiles.push_back(v);
+
+			int i;
+
+			while (ss >> i)
+			{
+				mapData.tiles[numLayers].push_back(i);
+
+				if (ss.peek() == ',')
+				{
+					ss.ignore();
+				}
+			}
+
+			pLayerElement = pLayerElement->NextSiblingElement("layer");
+		}
+
+		// parse object groups
+		XMLElement* pObjectGroupElement = pMapElement->FirstChildElement("objectgroup");
 
 		while (pObjectGroupElement != nullptr)
 		{
@@ -206,6 +202,13 @@ int parseTmx(const char* tmxFile, const char* topPathToMatch, const char* outTab
 
 			if (strcmp(name, "NoWalk") == 0)
 			{
+				// create walkability grid the first time we encounter a layer with this name.
+				if (mapData.walkabilityGrid == nullptr)
+				{
+					mapData.walkabilityGrid = new bool[mapData.mapLength()];
+					memset(mapData.walkabilityGrid, 1, mapData.mapLength());
+				}
+
 				XMLElement* pListElement = pObjectGroupElement->FirstChildElement("object");
 
 				while (pListElement != nullptr)
@@ -234,18 +237,17 @@ int parseTmx(const char* tmxFile, const char* topPathToMatch, const char* outTab
 
 			pObjectGroupElement = pObjectGroupElement->NextSiblingElement("objectgroup");
 		}
+
+		// write file
+		log("Encoding tmx as lua script...");
+		writeLuaMapScript(mapData, outScript);
+
+		return XML_SUCCESS;
 	}
 
-	// write file
-	log("Encoding tmx as lua script...");
-	writeLuaMapScript(mapData, outScript);
-
-	return XML_SUCCESS;
-}
-
-void readTmx(const char* tmxFile, const char* topPathToMatch, const char* outTableName, string& outScript)
-{
-	XMLCheckResult(parseTmx(tmxFile, topPathToMatch, outTableName, outScript));
-}
+	void readTmx(const char* tmxFile, const char* topPathToMatch, const char* outTableName, string& outScript)
+	{
+		XMLCheckResult(parseTmx(tmxFile, topPathToMatch, outTableName, outScript));
+	}
 
 } // end of namespace mingine 
