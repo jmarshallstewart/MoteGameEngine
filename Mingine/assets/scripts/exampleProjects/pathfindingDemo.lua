9c8114ad4913c7190134da45ab18dd7ae7ab8608
James <jmarshallstewart@gmail.com>
2018-03-04 12:12:15 -0600

    pathfindingDemo release1
    1. Adds extensive comments for how to use demo.
    2. Refactors script--mostly decomposing big functions into little functions.
    3. Removes a nil check is save function that is no longer needed.
    4. Adds a new folder for waypoint data to assets.

--- Mingine/assets/scripts/exampleProjects/pathfindingDemo.lua
+++ Mingine/assets/scripts/exampleProjects/pathfindingDemo.lua
@@ -1,3 +1,43 @@
+--[[
+This example is a waypoint editor with a PIE ("play in editor") mode. This tool allows the user to
+author a graph of waypoints and the connections between them. The user can save their work and
+reload it to continue editing in another session. The PIE mode creates a single agent that will
+use the waypoint graph to find paths to wherever the user clicks. 
+
+The demo has two modes: one for laying down a graph of waypoints, and another
+for testing the graph by having an agent find and execute paths.
+
+*********************************
+Waypoint Authoring Mode Controls:
+
+Left mouse click: if it would not overlap an existing waypoint, add a new waypoint at the click position.
+
+Right mouse click and drag: drag to reposition existing waypoints. Waypoint will be moved to 
+the cursor position at which the right mouse button was released.
+
+Left mouse click and drag: If drag starts over a waypoint and ends over a different waypoint,
+add a two-way connection between the waypoints. Left click and dragging over two connected waypoints removes
+their connection.
+
+delete key: if the mouse cursor is positioned over a waypoint, that waypoint will be deleted along with
+any connections to that waypoint.
+
+s key: Pressing the s key will save the current waypoint graph to the file specified 
+in the lua variable waypointsFilePath, below. This is a silent operation with
+no feedback to the user as to whether the save was successful. This demo will also
+load waypoint data on start-up if the file is present at waypointsFilePath.
+
+********************************
+Agent Pathfinding Demo Controls:
+
+Left click: Agent will use the waypoint graph as a search space to find a path
+to the position of the cursor when the left mouse button was clicked. The agent
+will begin its path at the nearest waypoint, and end its movement when it reaches
+the waypoint nearest to the click position. If the agent cannot find a path, nothing
+happens.
+
+]]--
+
 dofile(scriptDirectory .. "core/steering.lua")
 dofile(scriptDirectory .. "core/pathfinding.lua")
 
@@ -10,10 +50,13 @@ MAX_ACCELERATION = 1.0 -- how fast can the agent change direction and speed?
 --user interface state
 MODE_WAYPOINT_AUTHORING = 1
 MODE_AGENT_PATHFINDING_DEMO = 2
+prevLeftMouseButtonDown = false
 isLeftClickDragging = false
 selectedWaypoint = nil
 mode = MODE_WAYPOINT_AUTHORING
-waypointsFilePath = scriptDirectory .. "exampleProjects/graph.txt"
+waypointsFilePath = assetDirectory .. "/waypoints/" .. "pathfindingDemoGraph.lua"
+
+-- helper functions:
 
 --assumes nodes have x, y position and edges have start and finish indices into nodes list.
 function GenerateWaypointGraph(nodes, edges)
@@ -36,6 +79,112 @@ function GenerateWaypointGraph(nodes, edges)
     end
     
     return graph
+end
+
+-- return the index of the nearest waypoint in the table waypoints
+function GetNearestWaypoint(x, y)
+    local nearest = nil
+    
+    if #waypoints > 0 then
+        nearest = 1
+    end
+    
+    for i = 2, #waypoints do
+        -- we only care about relative distance, so we can skip the sqrt() call we normally
+        -- make when calculating distance, hence the call to distanceSquared().
+        local distanceSquared = DistanceSquared(x, y, waypoints[i].x, waypoints[i].y)
+        
+        if distanceSquared < DistanceSquared(x, y, waypoints[nearest].x, waypoints[nearest].y) then
+            nearest = i
+        end
+    end
+    
+    return nearest
+end
+
+function CreateWaypointAt(x, y)
+    local waypoint = { x = x, y = y}
+    waypoints[#waypoints + 1] = waypoint
+end
+
+function RemoveWaypointAt(x, y)
+    for i = 1, #waypoints do
+        if IsOnWaypoint(x, y, waypoints[i]) then
+            --remove links associated with removed waypoint.
+            for j = #links, 1, -1 do
+                if (links[j].start == i) or (links[j].finish == i) then
+                    table.remove(links, j)
+                end
+            end
+            
+            table.remove(waypoints, i)
+            
+            --lower indices in existing links since waypoints above that index have
+            --been re-indexed.
+            for k = 1, #links do
+                if links[k].start > i then links[k].start = links[k].start - 1 end                            
+                if links[k].finish > i then links[k].finish = links[k].finish - 1 end
+            end
+            
+            break
+        end
+    end
+end
+
+-- load waypoints from the save file, or create
+-- a fresh empty list if the save file does not
+-- exist.
+function InitializeWaypoints()
+    if IsFileReadable(waypointsFilePath) then
+        LoadWaypointsFile()
+    else
+        --when reading about A* or graphs in general, waypoints and links
+        --are referred to as nodes and edges.
+        waypoints = {}
+        links = {}
+    end
+end
+
+function IsOnWaypoint(x, y, waypoint)
+    return IsPointInCircle(x, y, waypoint.x, waypoint.y, ARRIVE_DISTANCE)
+end
+
+-- agent functions:
+
+function ResetAgent()
+    agent.isPathing = false
+    agent.path = {}
+    agent.velocity.x = 0
+    agent.velocity.y = 0
+    agent.acceleration.x = 0
+    agent.acceleration.y = 0
+    agent.targetWaypoint = 1
+end
+
+-- after the agent has a path, this function is
+-- used to make the agent actually follow along that
+-- path toward its goal.
+function UpdateAgentPathfollowingBehavior()
+    --get the next waypoint along the path
+    local next = agent.path[agent.targetWaypoint]
+
+    --next waypoint on the path is our seek target. 
+    --accelerate toward that.
+    local x, y = Seek(agent, next.x, next.y)
+    Set(agent.acceleration, x, y, MAX_ACCELERATION)
+            
+    --the angle of our sprite should match its own velocity    
+    TurnTo(agent, agent.velocity)
+    UpdateEntity(agent)
+    
+    --if agent is close enough to the current waypoint, set the next 
+    --waypoint as the new target (wrapping back to the first waypoint if needed)
+    if Distance(agent.x, agent.y, next.x, next.y) <= ARRIVE_DISTANCE then
+        agent.targetWaypoint = agent.targetWaypoint + 1
+        if agent.targetWaypoint > #agent.path then
+            ResetAgent()
+        end
+    end
 end
 
 function FindPath(start, finish)
@@ -80,36 +229,7 @@ function TryStartPathfinding(goalX, goalY)
     end
 end
 
--- return the index of the nearest waypoint in the table waypoints
-function GetNearestWaypoint(x, y)
-    local nearest = nil
-    
-    if #waypoints > 0 then
-        nearest = 1
-    end
-    
-    for i = 2, #waypoints do
-        -- we only care about relative distance, so we can skip the sqrt() call we normally
-        -- make when calculating distance, hence the call to distanceSquared().
-        local distanceSquared = DistanceSquared(x, y, waypoints[i].x, waypoints[i].y)
-        
-        if distanceSquared < DistanceSquared(x, y, waypoints[nearest].x, waypoints[nearest].y) then
-            nearest = i
-        end
-    end
-    
-    return nearest
-end
-
-function ResetAgent()
-    agent.isPathing = false
-    agent.path = {}
-    agent.velocity.x = 0
-    agent.velocity.y = 0
-    agent.acceleration.x = 0
-    agent.acceleration.y = 0
-    agent.targetWaypoint = 1
-end
+-- save/load waypoint graph file helper functions:
 
 function LoadWaypointsFile()
     local file = io.open(waypointsFilePath, "r")
@@ -118,37 +238,40 @@ function LoadWaypointsFile()
     file:close()
 end
 
-function WriteWaypointsFile()
+function SaveWaypointsFile()
     local file = io.open(waypointsFilePath, "w")
-            
-    if waypoints ~= nil and links ~= nil then
-        file:write("waypoints = {\n")
         
-        for n = 1, #waypoints do
-            file:write("    { x = ", waypoints[n].x, ", y = ", waypoints[n].y, " },\n")
-        end
-        
-        file:write("}\n")
-        
-        file:write("\nlinks = {\n")
-        
-        for e = 1, #links do
-            file:write("    { start = ", links[e].start, ", finish = ", links[e].finish, "},\n")
-        end
-        
-        file:write("}\n")
-                
-        file:close()
+    -- save waypoints
+    file:write("waypoints = {\n")
+    
+    for n = 1, #waypoints do
+        file:write("    { x = ", waypoints[n].x, ", y = ", waypoints[n].y, " },\n")
     end
+    
+    file:write("}\n")
+    
+    --save links
+    file:write("\nlinks = {\n")
+    
+    for e = 1, #links do
+        file:write("    { start = ", links[e].start, ", finish = ", links[e].finish, "},\n")
+    end
+    
+    file:write("}\n")
+            
+    file:close()
 end
 
+-- per-mode update functions:
+
+-- updates the waypoint authoring mode when that mode is active.
 function UpdateModeWaypointAuthoring()
     local mouseX, mouseY = GetMousePosition()
 
     -- left click
     if IsMouseButtonDown(1) then
         for i = 1, #waypoints do
-            if IsPointInCircle(mouseX, mouseY, waypoints[i].x, waypoints[i].y, ARRIVE_DISTANCE) then
+            if IsOnWaypoint(mouseX, mouseY, waypoints[i]) then
                 if not isLeftClickDragging then
                     isLeftClickDragging = true
                     startDragWaypoint = i
@@ -158,15 +281,12 @@ function UpdateModeWaypointAuthoring()
         end
         
         if not isLeftClickDragging then
-            local waypoint = {}
-            waypoint.x = mouseX
-            waypoint.y = mouseY
-            waypoints[#waypoints + 1] = waypoint
+            CreateWaypointAt(mouseX, mouseY)
         end
     elseif isLeftClickDragging then
         isLeftClickDragging = false
         for i = 1, #waypoints do
-            if IsPointInCircle(mouseX, mouseY, waypoints[i].x, waypoints[i].y, ARRIVE_DISTANCE) then
+            if IsOnWaypoint(mouseX, mouseY, waypoints[i]) then
                 if i ~= startDragWaypoint then
                     for j = 1, #links do
                         if (links[j].start == startDragWaypoint and links[j].finish == i) or (links[j].start == i and links[j].finish == startDragWaypoint) then
@@ -186,7 +306,7 @@ function UpdateModeWaypointAuthoring()
     if IsMouseButtonDown(3) then
         if selectedWaypoint == nil then
             for i = 1, #waypoints do
-                if IsPointInCircle(mouseX, mouseY, waypoints[i].x, waypoints[i].y, ARRIVE_DISTANCE) then
+                if IsOnWaypoint(mouseX, mouseY, waypoints[i]) then
                     selectedWaypoint = waypoints[i]
                     break;
                 end
@@ -199,60 +319,34 @@ function UpdateModeWaypointAuthoring()
         selectedWaypoint = nil
     end
     
-    -- delete
+    -- handle keyboard input
     if IsKeyPressed(SDL_SCANCODE_DELETE) then
-        for i = 1, #waypoints do
-            if IsPointInCircle(mouseX, mouseY, waypoints[i].x, waypoints[i].y, ARRIVE_DISTANCE) then
-                --remove links associated with removed waypoint.
-                for j = #links, 1, -1 do
-                    if (links[j].start == i) or (links[j].finish == i) then
-                        table.remove(links, j)
-                    end
-                end
-                
-                table.remove(waypoints, i)
-                
-                --lower indices in existing links since waypoints above that index have
-                --been re-indexed.
-                for k = 1, #links do
-                    if links[k].start > i then links[k].start = links[k].start - 1 end                            
-                    if links[k].finish > i then links[k].finish = links[k].finish - 1 end
-                end
-                
-                break
-            end
-        end
+        RemoveWaypointAt(mouseX, mouseY)
     elseif IsKeyPressed(SDL_SCANCODE_S) then
-        WriteWaypointsFile()
+        SaveWaypointsFile()
     end
 end
 
+-- updates the PIE mode when that mode is active.
 function UpdateModeAgentDemo()
-    
-    if agent.isPathing then
-        --get the next waypoint along the path
-        local next = agent.path[agent.targetWaypoint]
-
-        --next waypoint on the path is our seek target. 
-        --accelerate toward that.
-        local x, y = Seek(agent, next.x, next.y)
-        Set(agent.acceleration, x, y, MAX_ACCELERATION)
-                
-        --the angle of our sprite should match its own velocity    
-        TurnTo(agent, agent.velocity)
-        UpdateEntity(agent)
+    local leftMousePressed = not prevLeftMouseButtonDown and IsMouseButtonDown(1)
         
-        --if agent is close enough to the current waypoint, set the next 
-        --waypoint as the new target (wrapping back to the first waypoint if needed)
-        if Distance(agent.x, agent.y, next.x, next.y) <= ARRIVE_DISTANCE then
-            agent.targetWaypoint = agent.targetWaypoint + 1
-            if agent.targetWaypoint > #agent.path then
-                ResetAgent()
-            end
-        end
-    elseif IsMouseButtonDown(1) then -- left click
+    if agent.isPathing and not leftMousePressed then
+        UpdateAgentPathfollowingBehavior()
+    elseif leftMousePressed then -- left click
         local mouseX, mouseY = GetMousePosition()
         TryStartPathfinding(mouseX, mouseY)
+    end
+end
+
+-- Draw() helper functions:
+
+function DrawPath(agent)
+    if agent.path ~= nil then
+        SetDrawColor(255, 0, 255, 255)
+        for i = 1, #agent.path do
+            DrawCircle(agent.path[i].x, agent.path[i].y, ARRIVE_DISTANCE)
+        end
     end
 end
 
@@ -292,27 +386,22 @@ end
 
 function Start()
     CreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
-    SetWindowTitle("Pathfinding Example -- Press TAB to toggle authoring mode.")
+    SetWindowTitle("Waypoint Editor -- Press TAB to toggle PIE mode.")
     
     font = LoadFont("fonts/8_bit_pusab.ttf", 16)
     
+    -- create pathfollowing agent
     local image = LoadImage("images/arrow.png")
     agent = CreateEntity(image, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 32, 16)
     agent.maxSpeed = 8
     agent.path = {}
     agent.targetWaypoint = 1
     
-    --when reading about A*, waypoints and links are nodes and edges.
-    if IsFileReadable(waypointsFilePath) then
-        LoadWaypointsFile()
-    else
-        waypoints = {}
-        links = {}
-    end
+    InitializeWaypoints()
 end
 
 function Update()
-    --test whether we should switch between modes, otherwise just update the current mode.
+    --test whether we should switch between modes, but otherwise just update the current mode.
     if IsKeyPressed(SDL_SCANCODE_TAB) then
         ResetAgent()
         
@@ -327,23 +416,19 @@ function Update()
     elseif mode == MODE_AGENT_PATHFINDING_DEMO then
         UpdateModeAgentDemo()
     end 
+    
+    prevLeftMouseButtonDown = IsMouseButtonDown(1)
 end
 
 function Draw()
     ClearScreen(68, 136, 204)
     
     DrawWaypointGraph()
-    
-    -- draw path
-    if agent.path ~= nil then
-        SetDrawColor(255, 0, 255, 255)
-        for i = 1, #agent.path do
-            DrawCircle(agent.path[i].x, agent.path[i].y, ARRIVE_DISTANCE)
-        end
+       
+    if mode == MODE_AGENT_PATHFINDING_DEMO then
+        DrawPath(agent)
+        DrawEntity(agent)
     end
-    
-    -- draw pathfinding agent.
-    DrawEntity(agent)
         
     DrawModeText()
 end
